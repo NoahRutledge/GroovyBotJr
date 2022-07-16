4fd03d40e3ad1e3d28299b2e5a11bdfab6205753
NoahRutledge <noah.rutledge@gmail.com>
2022-07-16 19:13:29 -0400

    Creating music handler helper to offload music function from the rest

---
+++ MusicHandler.ts
@@ -0,0 +1,123 @@
+﻿import { entersState, joinVoiceChannel, VoiceConnectionStatus } from '@discordjs/voice';
+import { validateURL } from 'ytdl-core';
+
+import Discord, { GuildMember, MessagePayload, Snowflake } from 'discord.js';
+
+import { Track } from './Music/Track';
+import { MusicSubscription } from './Music/Subscription';
+import { Subscriptions } from './groovy';
+
+const ytsr = require('ytsr');
+const YoutubeSearchOptions =
+{
+	limit: 1,
+	safeSearch: false,
+};
+
+export async function HandleMusicCommand(command: string, args: string[], message: Discord.Message)
+{
+	let subscription = Subscriptions.get(message.guildId);
+	if (!subscription) {
+		switch (command) {
+			case 'play':
+				subscription = TryEnterChannel(message);
+				break;
+			default:
+				return;
+		}
+	}
+
+	switch (command)
+	{
+		case 'play':
+
+			if (args.length == 1) {
+				message.channel.send("Please supply an argument");
+				return;
+			}
+
+			try {
+				var requestMessage = message.content.substring(6);
+				var requestUrl = "";
+				if (validateURL(requestMessage) == false) {
+					const searchResult = await ytsr(requestMessage, YoutubeSearchOptions);
+					requestUrl = searchResult.items[0].url;
+				}
+				else {
+					requestUrl = requestMessage;
+				}
+
+				const track = await Track.From(requestUrl, requestMessage, message.channel);
+				subscription.Enqueue(track);
+				var m = await message.channel.send('Enqueued "' + track.Title + '"')
+				track.AddMessage(m);
+
+			} catch (error) {
+				console.warn(error);
+				message.channel.send('Failed to play track!');
+			}
+			break;
+
+		case 'stop':
+			subscription.Stop();
+			break;
+
+		case 'skip':
+			subscription.Skip();
+			break;
+
+		case 'resume':
+			subscription.Resume();
+			break;
+
+		case 'pause':
+			subscription.Pause()
+			break;
+
+		case 'disconnect':
+			subscription.Destroy();
+			Subscriptions.delete(message.guildId);
+			break;
+
+		case 'remove':
+			var songToRemove = message.content.substring(8);
+			subscription.RemoveFromQueue(songToRemove);
+			break;
+    }
+}
+
+export function TryEnterChannel(message: Discord.Message): MusicSubscription {
+	var subscription = Subscriptions.get(message.guildId);
+	if (!subscription) {
+		subscription = CreateChannelSubscription(message);
+		if (!subscription)
+			return null;
+	}
+
+	try {
+		entersState(subscription.voiceConnection, VoiceConnectionStatus.Ready, 20e3);
+		return subscription;
+	}
+	catch (error) { console.warn(error); return null; }
+}
+
+function CreateChannelSubscription(message: Discord.Message): MusicSubscription {
+	const channel = message.member.voice.channel;
+	if (message.member instanceof GuildMember && channel) {
+		const subscription = new MusicSubscription
+			(
+				joinVoiceChannel({
+					channelId: channel.id,
+					guildId: channel.guild.id,
+					adapterCreator: channel.guild.voiceAdapterCreator,
+				})
+			);
+		subscription.voiceConnection.on('error', console.warn);
+		Subscriptions.set(message.guildId, subscription);
+		return subscription;
+	}
+	else {
+		message.channel.send('You need to join a voice channel first!');
+		return null;
+	}
+}
--- groovy.ts
+++ groovy.ts
@@ -1,24 +1,12 @@
-import Discord, { GuildMember, Snowflake } from 'discord.js';
-import 
-{
-	entersState,
-	joinVoiceChannel,
-	VoiceConnectionStatus
-} from '@discordjs/voice';
-import { Track } from './Music/Track';
+import Discord, { Snowflake } from 'discord.js';
+import { HandleMusicCommand, TryEnterChannel } from './MusicHandler';
 import { MusicSubscription } from './Music/Subscription';
-import { validateURL } from 'ytdl-core';
-const ytsr = require('ytsr');
 
 const bot = new Discord.Client({intents: ['GUILDS', 'GUILD_VOICE_STATES', 'GUILD_MESSAGES']});
 const { token } = require('../auth.json');
+export const Subscriptions = new Map<Snowflake, MusicSubscription>();
 
-const Subscriptions = new Map<Snowflake, MusicSubscription>();
-const YoutubeSearchOptions = 
-{
-	limit: 1,
-	safeSearch: false,
-};
+const MUSIC_COMMANDS = ["play", "skip", "disconnect", "stop", "resume", "remove"];
 
 bot.on('ready', () => console.log('Ready!'));
 
@@ -30,83 +18,16 @@ bot.on('messageCreate', async (message) =>
 		const args = message.content.substring(1).split(' ');
 		const command = args[0].toLowerCase();
 
-		let subscription = Subscriptions.get(message.guildId);
-		if(!subscription)
+		if (MUSIC_COMMANDS.includes(command))
 		{
-			switch(command)
-			{
-				case 'play':
-				case 'gimmethedick':
-				case 'lemmeticklethefeet':
-					subscription = TryEnterChannel(message);
-					break;
-				default:
-					return;
-			}
-		}
+			HandleMusicCommand(command, args, message);
+			return;
+        }
 
 		switch(command)
 		{
-			case 'play':
-			
-				if(args.length == 1)
-				{
-					message.channel.send("Please supply an argument");
-					return;
-				}
-
-				try
-				{
-					var requestMessage = message.content.substring(6);
-					var requestUrl = "";
-					if(validateURL(requestMessage) == false)
-					{
-						const searchResult = await ytsr(requestMessage, YoutubeSearchOptions);
-						requestUrl = searchResult.items[0].url;
-					}
-					else
-					{
-						requestUrl = requestMessage;
-					}
-
-					const track = await Track.From(requestUrl, requestMessage, message.channel);
-					subscription.Enqueue(track);
-					var m = await message.channel.send('Enqueued "' + track.Title + '"')
-					track.AddMessage(m);
-
-				} catch(error)
-				{
-					console.warn(error);
-					message.channel.send('Failed to play track!');
-				}
-				break;
-
-			case 'stop':
-				subscription.Stop();
-				break;
-
-			case 'skip':
-				subscription.Skip();
-				break;
-
-			case 'resume':
-			case 'unpause':
-			case 'start':
-				subscription.Resume();
-				break;
-
-			case 'pause':
-				subscription.Pause()
-				break;
-
-			case 'disconnect':
-				subscription.Destroy();
-				Subscriptions.delete(message.guildId);
-				break;
-
-			case 'remove':
-				var songToRemove = message.content.substring(8);
-				subscription.RemoveFromQueue(songToRemove);
+			case 'doubt':
+				message.channel.send({files: ["./images/doubt.jpg"]});
 				break;
 
 			default:
@@ -121,47 +42,5 @@ bot.on('messageCreate', async (message) =>
 		TryEnterChannel(message);
 	}
 });
-
-function TryEnterChannel(message: Discord.Message): MusicSubscription
-{
-	var subscription = Subscriptions.get(message.guildId);
-	if(!subscription)
-	{
-		subscription = CreateChannelSubscription(message);
-		if(!subscription)
-			return null;
-	}
-
-	try
-	{
-		entersState(subscription.voiceConnection, VoiceConnectionStatus.Ready, 20e3);
-		return subscription;
-	} 
-	catch(error){ console.warn(error); return null; }
-}
-
-function CreateChannelSubscription(message: Discord.Message): MusicSubscription
-{
-	const channel = message.member.voice.channel;
-	if(message.member instanceof GuildMember && channel)
-	{
-		const subscription = new MusicSubscription
-		(
-			joinVoiceChannel({
-				channelId: channel.id,
-				guildId: channel.guild.id,
-				adapterCreator: channel.guild.voiceAdapterCreator,
-			})
-		);
-		subscription.voiceConnection.on('error', console.warn);
-		Subscriptions.set(message.guildId, subscription);
-		return subscription;
-	}
-	else
-	{
-		message.channel.send('You need to join a voice channel first!');
-		return null;
-	}
-}
 
 bot.login(token);
